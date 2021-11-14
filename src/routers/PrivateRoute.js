499d6244242acf656eece804a7c57a693d9bc88b
Mayank Bucha <47553721+mayankbucha@users.noreply.github.com>
2021-11-15 00:36:30 +0530

    Store user data in user area

--- src/routers/PrivateRoute.js
+++ src/routers/PrivateRoute.js
@@ -1,27 +1,27 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import {Redirect, Route} from 'react-router-dom';
-import {Header} from '../components/Header';
+import { connect } from 'react-redux';
+import { Route, Redirect } from 'react-router-dom';
+import Header from '../components/Header';
 
-export const PrivateRoute = ({isAuthenticated, 
-    component: Component,
-    ...rest
+export const PrivateRoute = ({
+  isAuthenticated,
+  component: Component,
+  ...rest
 }) => (
     <Route {...rest} component={(props) => (
-        isAuthenticated ? (
-            <div>
-                <Header />
-                <Component {...props} />
-            </div>
-        ) : (
-            // React component
-            <Redirect to='/' />
+      isAuthenticated ? (
+        <div>
+          <Header />
+          <Component {...props} />
+        </div>
+      ) : (
+          <Redirect to="/" />
         )
     )} />
-);
+  );
 
 const mapStateToProps = (state) => ({
-    isAuthenticated: !!state.auth.uid
+  isAuthenticated: !!state.auth.uid
 });
 
 export default connect(mapStateToProps)(PrivateRoute);
